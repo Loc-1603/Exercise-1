a87c08b52773064b03f6287c966fb2926d030656
Loc-1603 <hoxuantailoc12345@gmail.com>
2026-04-15 22:34:08 +0700

    fix

--- BLL/IDangKyService.cs
+++ BLL/IDangKyService.cs
@@ -1,14 +1,1 @@
-﻿using Entities;
-using System.Collections.Generic;
-
-namespace BLL
-{
-    public interface IDangKyService
-    {
-        List<DangKy> LayTatCaDangKy();
-        List<DangKy> LayDangKyTheоSinhVien(string maSV);
-        bool DangKyMonHoc(string maSV, string maMH);
-        bool NhapDiem(string maSV, string maMH, double diem);
-        double TinhDiemTrungBinh(string maSV);
-    }
-}
+﻿
--- BLL/ISinhVienService.cs
+++ BLL/ISinhVienService.cs
@@ -1,14 +1,1 @@
-﻿using Entities;
-using System.Collections.Generic;
-
-namespace BLL
-{
-    public interface ISinhVienService
-    {
-        List<SinhVien> LayTatCaSinhVien();
-        SinhVien LaySinhVienTheoMa(string maSV);
-        bool ThemSinhVien(SinhVien sv);
-        bool CapNhatSinhVien(string id, SinhVien sv);
-        bool XoaSinhVien(string id);
-    }
-}
+﻿
