56debbbe189783ef617d5b409a6f82d2ea117b6c
Minh Tuan <godxdemon567@gmail.com>
2026-04-15 19:34:57 +0700

    BLL

---
+++ BLL/IDangKyService.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public interface IDangKyService
+    {
+        List<DangKy> LayTatCaDangKy();
+        List<DangKy> LayDangKyTheоSinhVien(string maSV);
+        bool DangKyMonHoc(string maSV, string maMH);
+        bool NhapDiem(string maSV, string maMH, double diem);
+        double TinhDiemTrungBinh(string maSV);
+    }
+}
---
+++ BLL/IMonHocService.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public interface IMonHocService
+    {
+        List<MonHoc> LayTatCaMonHoc();
+        MonHoc LayMonHocTheoMa(string maMH);
+        bool ThemMonHoc(MonHoc mh);
+        bool XoaMonHoc(string id);
+    }
+}
