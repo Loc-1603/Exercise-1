88bea1c64332433e35ab3132ebd38bc074404356
KKien2005 <suprominhkien@gmail.com>
2026-04-15 22:08:03 +0700

    update GUI

---
+++ Exercise-1-main/Entities/DangKy.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Entities
+{
+    public class DangKy
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("maSV")]
+        public string MaSV { get; set; } = string.Empty;
+
+        [BsonElement("maMH")]
+        public string MaMH { get; set; } = string.Empty;
+
+        [BsonElement("ngayDangKy")]
+        public DateTime NgayDangKy { get; set; }
+
+        [BsonElement("diem")]
+        public double? Diem { get; set; }
+    }
+}
